b86ab5d124dd5786fdf7ef5216bd32b7a318ee9e
Michael Krause <michael.j.krause@gmail.com>
2022-09-29 06:49:18 -0700

    WIP

---
+++ RemoveNthNodeFromLinkedList/src/Node.java
@@ -0,0 +1,15 @@
+public class Node {
+    int val;
+    Node next;
+
+    Node() {}
+
+    Node(int val) {
+        this.val = val;
+    }
+
+    Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
+}
